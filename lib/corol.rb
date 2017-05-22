b8dcfb7e54dbf315f074ebe72a46897054e77add
Farhad Eyvazli <farhad9801@gmail.com>
2017-05-22 21:35:50 +0400

    added comments

--- lib/corol.rb
+++ lib/corol.rb
@@ -1,6 +1,7 @@
 module Corol
   colors = {
-      RED:"\e[31m",
+  #### Text Colors Codes ####
+      RED:"\e[31m",                    
       WHITE:"\e[0m",
       BLACK:"\e[30m",
       GREEN:"\e[32m",
@@ -16,6 +17,7 @@ module Corol
       LIGHT_CYAN:"\e[96m"
   }
   back_colors = {
+  #### BackGround Colors Codes ####
       BACK_RED:"\e[41m",
       BACK_GREEN:"\e[42m",
       BACK_YELLOW:"\e[43m",
@@ -30,20 +32,21 @@ module Corol
       BACK_LIGHT_CYAN:"\e[106m"
   }
   formatting = {
+  #### Formatting Codes ####
       BOLD:"\e[1m",
       DIM:"\e[2m",
       UND_LINE:"\e[4m",
       HIDDEN:"\e[8m"
   }
-  arr = [colors,back_colors,formatting]
+  hash_lists = [colors,back_colors,formatting] #array defined for store all hashs
 
-  arr.size.times do |l|
-    arr[l].keys.size.times do |i|
-      define_method(arr[l].keys[i].downcase) do
-        return arr[l].values[i]+"#{self}"+"\e[0m"
+  hash_lists.size.times do |l|
+    hash_lists[l].keys.size.times do |i|
+      define_method(hash_lists[l].keys[i].downcase) do   # This will define 30 methods with all hash keys . And we downcased them. 
+        return hash_lists[l].values[i]+"#{self}"+"\e[0m" # Every line have to  end with "\e[0m" . Its also color code for white.
       end
     end
   end
 end
 
-class String ; include Corol ; end
+class String ; include Corol ; end #we include module to use methods as instance methods
